1b30c6e5120f9effaba1061cda44ff4b841dca42
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-22 00:03:51 -0800

    Create RemoveContact Button Component

---
+++ client/src/components/buttons/RemoveContact.js
@@ -0,0 +1,10 @@
+import { DeleteOutlined } from "@ant-design/icons"
+
+
+const RemoveContact = () => {
+    return (
+        <DeleteOutlined key='delete' style={{color: 'red'}} />
+    )
+}
+
+export default RemoveContact
