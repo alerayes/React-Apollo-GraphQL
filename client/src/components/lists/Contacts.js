1fbd1d283378255ae9dbdb54ead71bae6ba3505f
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-22 00:53:23 -0800

    Fix Bugs and Update Variable States

--- client/src/components/lists/Contacts.js
+++ client/src/components/lists/Contacts.js
@@ -1,40 +1,32 @@
-import { useQuery } from "@apollo/client"
-import { List } from "antd"
-import { GET_CONTACTS } from "../../queries"
-import ContactCard from "../listitems/ContactCard"
+import { useQuery } from '@apollo/client'
+import { List } from 'antd'
+import { GET_CONTACTS } from '../../queries'
+import ContactCard from '../listitems/ContactCard'
 
 const getStyles = () => ({
-    list: {
-        display: 'flex',
-        justifyContent: 'center'
-    }
+  list: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
 })
 
 const Contacts = () => {
-    const styles = getStyles()
+  const styles = getStyles()
 
-    const {loading, error, data} =  useQuery(GET_CONTACTS)
+  const { loading, error, data } = useQuery(GET_CONTACTS)
 
-    if(loading) return 'Loading...'
-    if(error) return `Error! ${error.message}`
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
 
-    return (
-        <List
-            grid={{ gutter: 20, column:1}}
-            style={styles.list}
-        >
-            {data.contacts.map(( {id, firstName, lastName}) => (
-                <List.Item key={id}>
-                    <ContactCard 
-                        key={id}
-                        id={id}
-                        firstName={firstName}
-                        lastName={lastName}
-                    />
-                </List.Item>
-            ))}
-        </List>
-    )
+  return (
+    <List grid={{ gutter: 20, column: 1 }} style={styles.list}>
+      {data.contacts.map(({ id, firstName, lastName }) => (
+        <List.Item key={id}>
+          <ContactCard key={id} id={id} firstName={firstName} lastName={lastName} />
+        </List.Item>
+      ))}
+    </List>
+  )
 }
 
 export default Contacts
