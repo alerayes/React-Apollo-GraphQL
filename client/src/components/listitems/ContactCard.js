a42a32df534de8d186897287960f84ac889d7759
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-22 00:02:58 -0800

    Update ContactCard Component With Props and EditMode

--- client/src/components/listitems/ContactCard.js
+++ client/src/components/listitems/ContactCard.js
@@ -1,4 +1,8 @@
 import { Card } from "antd"
+import {EditOutlined} from "@ant-design/icons"
+import RemoveContact from "../buttons/RemoveContact"
+import { useState } from "react"
+import UpdateContact from "../forms/UpdateContact"
 
 const getStyles = () => ({
     card: {
@@ -7,14 +11,36 @@ const getStyles = () => ({
 })
 
 
-const ContactCard = () => {
-
+const ContactCard = props => {
+    const {id, firstName, lastName} = props
     const styles = getStyles()
 
+    const [editMode, setEditMode] = useState(false)
+
+    const handleButtonClick = () => {
+        setEditMode(!editMode)
+    }
+
+    console.log('props', props)
+
     return(
-        <Card style={styles.card}>
-            Elon Musk
-        </Card>
+        <div>
+        {editMode ? (
+          <UpdateContact
+            onButtonClick={handleButtonClick}
+          />
+        ) : (
+          <Card
+            style={styles.card}
+            actions={[
+              <EditOutlined key='edit' onClick={handleButtonClick}/>,
+              <RemoveContact />
+            ]}
+          >
+            {firstName} {lastName}
+          </Card>
+        )}
+      </div>
     )
 }
 
